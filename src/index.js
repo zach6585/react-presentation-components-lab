d78b4e5157c3365b9fbb2606ecba2e85c54ec192
zach6585 <zmines@wesleyan.edu>
2021-04-06 16:38:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={style = "font-color: green;"} />
   </div>,
   document.getElementById('root')
 );
